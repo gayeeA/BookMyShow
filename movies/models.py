93ea0d1759400cf1bc0e0a478fee49a0b475c056
GAYATHRI A <145894167+gayeeA@users.noreply.github.com>
2024-12-25 22:28:58 +0530

    models changes

--- movies/models.py
+++ movies/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Movie(models.Model):
     name= models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
     image= models.ImageField(upload_to="movies/")
     rating = models.DecimalField(max_digits=3,decimal_places=1)
     cast= models.TextField()
@@ -29,10 +30,10 @@ class Seat(models.Model):
         return f'{self.seat_number} in {self.theater.name}'
 
 class Booking(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
     seat=models.OneToOneField(Seat,on_delete=models.CASCADE)
-    movie=models.ForeignKey(Movie,on_delete=models.CASCADE)
     theater=models.ForeignKey(Theater,on_delete=models.CASCADE)
-    booked_at=models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='movies_bookings')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='movie_bookings')
+    booked_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f'Booking by{self.user.username} for {self.seat.seat_number} at {self.theater.name}'
--- users/models.py
+++ users/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 from movies.models import Movie
-import users
+
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Booking(models.Model):
-    user = models.ForeignKey(users, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users_bookings')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='users_movie_bookings')
     booked_at = models.DateTimeField(auto_now_add=True)
