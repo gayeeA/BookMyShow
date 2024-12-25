e14b7ec2b245064e03d9b09f99336bcf0522c343
GAYATHRI A <145894167+gayeeA@users.noreply.github.com>
2024-12-25 22:34:01 +0530

    model error rectified

--- users/models.py
+++ users/models.py
@@ -6,6 +6,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Booking(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users_bookings')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='users_movie_bookings')
     booked_at = models.DateTimeField(auto_now_add=True)
